aed31ca251c5bd7c9f5cee8dca09e46f23c12ddf
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2021-04-07 07:48:52 +0700

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,17 +1,15 @@
-from losses import CenternetLoss
 import torch
 import torch.nn as nn
 from focal_loss.focal_loss import FocalLoss
 
 class ORGFocalLoss(nn.Module):
     def __init__(self, gamma=4, alpha=2):
-        super(ORGFocalLoss2, self).__init__()
+        super(ORGFocalLoss, self).__init__()
         from focal_loss.focal_loss import FocalLoss
         self.focal_loss = FocalLoss(gamma=gamma, alpha=alpha, reduction='mean')
     def forward(self, pred, gt):
         out = self.focal_loss(pred, gt)
         return out
-        return out.mean(axis=-1).mean(axis=-1).mean(axis=-1, keepdim=True)
 
 class L1MaskLoss(nn.Module):
     def __init__(self):
@@ -25,7 +23,6 @@ class CenternetLoss(nn.Module):
         super(CenternetLoss, self).__init__()
         self.size_fac = size_factor
         self.offset_fac = offset_factor
-        # self.focal_loss = FocalLoss()
         self.focal_loss = ORGFocalLoss(gamma=4, alpha=2)
         self.l1maskloss = L1MaskLoss()
 
@@ -33,4 +30,4 @@ class CenternetLoss(nn.Module):
         offset_loss = self.l1maskloss(gt_list[1], pred_list[1], mask)
         size_loss = self.l1maskloss(gt_list[2], pred_list[2], mask)
         heat_loss = self.focal_loss(pred_list[0], gt_list[0])
-        return heat_loss + self.offset_fac*offset_loss + self.size_fac*size_loss, heat_loss, offset_loss, size_loss
+        return heat_loss + self.offset_fac*offset_loss.mean() + self.size_fac*size_loss.mean(), heat_loss, offset_loss, size_loss
